aab9d7282f251ed6f4c82ed11a77dfec1eec3a55
Ibrahim Awad <ibrahim.a.hamid@gmail.com>
2018-12-04 01:14:14 -0800

    Copying master modifications into this branch

--- urls.py
+++ urls.py
@@ -1,12 +1,5 @@
-# from django.conf.urls import patterns, url, include
-# from django.views.generic import TemplateView
-# from . import views, APP_NAME
-#
-# urlpatterns = patterns('',
-#     url(r'^$', views.index, name='%s.index' % APP_NAME),
-# )
-from django.conf.urls import patterns, url
 import views
+
 from . import APP_NAME
 
 urlpatterns = views.dashboard.get_url_patterns()
